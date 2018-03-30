9828f49d4e1113c0f16f3de1c9b3928bca37f619
Jack Warner <jack.warner@parivedasolutions.com>
2018-03-30 14:43:12 -0400

    add auth and basic slack request

--- README.md
+++ README.md
@@ -4,25 +4,53 @@ A Slack bot for recording AWS certifications you've earned.
 
 ## Pre-reqs
 
+- You've deployed this service successfully (see https://github.com/pariveda-serverless/support/tree/master/create-new-service)
+- Get the API Gateway URL. You'll use it at the 'Request URL' configuration step:
+
+    ``` 
+    $ aws cloudformation describe-stacks --stack-name aws-certbot-master --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue' --output text
+    
+    Hint: stack-name is the service followed by a '-' then the stage/branch
+    ``` 
+    
 - You have a Slack development workspace
 - You've created a new app at https://api.slack.com/apps?new_app=1 in that development workspace
 - You've created a slash command at https://api.slack.com/apps/A9ZA995GF/slash-commands? (where 'A9ZA995GF' corresponds to the link for your development workspace)
-- The following values will work:
+
+- Enter The following values will work (append the function name to the API Gateway URL above):
 
     ``` 
     Command: /aws-cert
-    Request URL: http://google.com <- will change once we deploy the function
+    Request URL: https://xvwjphbae3.execute-api.us-east-1.amazonaws.com/master/hello/world
     Short Description: Log and share the AWS certs you've worked so hard to earn
     Usage Hint: [Validation Number]
     ```
 - At 'Oauth & Permissions' click 'Install App to Workspace' (https://api.slack.com/apps/A9ZA995GF/oauth?)
-- 
+- Save the value in ~/secrets/oauth.txt
+    
+    ``` 
+    echo -n "xoxp-xxxxxxxxx-xxxxxxx-yyyyyyy" > ~/secrets/oauth.txt
+    ```
+    
+- Encrypt and upload the file (see https://github.com/pariveda-serverless/support/tree/master/secrets-uploader for more details)
+
+    ``` 
+    ~/src/support/secrets-uploader $ docker-compose run --rm secrets
+    /var/aws $ ./upload.sh -service aws-certbot -stage master -path /var/secrets/oauth.txt -s3key oauth.enc
+    ```
+    
+    If you see something like 'Done - file saved to: aws-certbot-security-master-secretsbucket-b39999jbnza8/oauth.enc!' you are good to move on.
+
+- Create a 'bot user' at https://api.slack.com/apps/A9ZA995GF/bots?. The 
+
+
+- Use  to upload the fo
 
 ## Contributing
 
-- Fork the repository
-- Add cool stuff
-- Submit a pull request
-- Get credit!
+1. Fork the repository
+1. Add cool stuff
+1. Submit a pull request
+1. Get credit!
 
 
--- index.js
+++ index.js
@@ -1,43 +1,90 @@
-'use strict';
-
 const AWS = require('aws-sdk');
-var Lambda = require('lambda-log-wrapper');
-var Timer = require('lambda-log-timer');
+const docs = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const token = process.env['VERIFICATION_TOKEN'];
+const qs = require('querystring');
+const req = require('request');
 
-/* This is a sample Lambda Function for getting started with serverless
-*
-*/
-module.exports.hello = (event, context, callback) => {
-  Timer.getTimeService(main, event, context, callback);
-};
+const decryptedSlackAuthToken = process.env['SLACK_APP_AUTH_TOKEN'];
 
-function main(event, context, callback) {
-  // The code snippet here shows how you can access a value stored in the encrypted S3
-  // secrets bucket at runtime - see https://github.com/pariveda-serverless/support/tree/master/secrets-uploader
-  // for detailed instructions on uploading secret data
-  // const params = {
-  //     Bucket: process.env.SECRETS_BUCKET,
-  //     Key: 'secret-file.enc'
-  // };
-  // const s3 = new AWS.S3({signatureVersion: "v4"});
-  // s3.getObject(params, function(err, data) {
-  //     if (err) console.log(err, err.stack); // an error occurred
-  //     else {
-  //         const secretContents = data.Body.toString('ascii');
-  //         // Don't log secrets, because they will be pushed to Elastic/Kibana
-  //         // Instead, use them as you would any variable
-  //         console.log(secretContents);
-  //     }
-  // });
-  // end secret sample code
-  
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
+function getActivityDate() {
+    var dateObj = new Date();
+    var month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
+    var day = ('0' + dateObj.getDate()).slice(-2);
+    var year = dateObj.getFullYear();
+    var timestamp = month + "/" + day + "/" + year;
+    return timestamp;
 }
+
+function processEvent(event, context, callback) {
+    console.log(JSON.stringify(event, null, '  '));
+
+    var inputParams = qs.parse(event.body);
+    var requestToken = inputParams.token;
+    var slackUserId = inputParams.user_id;
+
+    if (requestToken != token) {
+        console.error("Request token (" + requestToken + ") does not match expected token for Slack");
+        context.fail("Invalid request token");
+    }
+
+    var slackValues = inputParams.text.split('%2C');
+    slackValues = inputParams.text.split(',');
+
+    var activityDate = new Date().toString();
+    var certid = (slackValues[0] !== null ? slackValues[0].toString() : "").trim();
+
+    req.post("https://slack.com/api/users.profile.get", {
+        auth: {
+            bearer: decryptedSlackAuthToken
+        },
+        form: {
+            token: decryptedSlackAuthToken,
+            user: slackUserId,
+            include_labels: false
+        }
+    }, function(error, response, body) {
+        if(error){
+            callback("SLACK PROFILE RETRIEVAL ERROR - " + error, null);
+        } else {
+            var slackInfo = JSON.parse(response.body);
+            console.log('slack info', slackInfo);
+            docs.put({
+                TableName: process.env.ACTIVITIES_TABLE,
+                Item : {
+                    timestamp: "" + new Date().getTime().toString(),
+                    userName: inputParams.user_name,
+                    name: slackInfo.profile.real_name,
+                    // level: slackInfo.profile.fields.Xf1M339XQX.value, // Level
+                    // office: slackInfo.profile.fields.Xf1LTXNG6P.value, // Office
+                    date: getActivityDate(),
+                    contact: contact,
+                    company: company,
+                    event: event,
+                    rawText: inputParams.text
+                }
+            }, function(err, data) {
+                if (err) {
+                    callback(err + " " + body.timestamp, null);
+                }
+                else {
+                    var successMessage = {
+                        "response_type": "in_channel",
+                        "text": "Success!"
+                        + "\n*Fin*: " + slackInfo.profile.real_name
+                        + "\n*Contact*: " + certid,
+                    };
+                    console.log('great success: ' + JSON.stringify(successMessage));
+                    var response = {
+                        statusCode: 200,
+                        body: JSON.stringify(successMessage)
+                    };
+                    callback(null, response);
+                }
+            });
+        }
+    });
+}
+
+exports.handler = (event, context, callback) => {
+    processEvent(event, context, callback);
+};
