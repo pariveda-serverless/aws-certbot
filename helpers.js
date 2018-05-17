f329f5f1efccfaceeb197ee3695b05f38046802c
Jack Warner <jack.warner@parivedasolutions.com>
2018-05-17 14:10:13 -0400

    Dave Allen

--- helpers.js
+++ helpers.js
@@ -143,6 +143,9 @@ function extractListingsFromHTML (html, certId, slackUserId, tableName, slackTok
         else if (likelyEmail === 'david.sulkin@parivedasolutions.com') {
             likelyEmail = 'dave.sulkin@parivedasolutions.com';
         }
+        else if (likelyEmail === 'dave.allen@parivedasolutions.com') {
+            likelyEmail = 'david.allen@parivedasolutions.com';
+        }
         // make sure the name is very likely to be a Fin
         req.post("https://slack.com/api/users.lookupByEmail", {
             auth: {
